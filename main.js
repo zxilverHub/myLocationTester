1601e1a46d54cdf06ce60e412711a8d3bd74f58c
Silver Dave Ramos <zilvdaveramos@gmail.com>
2024-01-27 22:26:06 +0800

    added btn

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const longitude = document.querySelector('#longitude'),
-      latitude = document.querySelector('#latitude');
+      latitude = document.querySelector('#latitude'),
+      getBtn = document.getElementById('get-btn');
 
-try {
-    navigator.geolocation.getCurrentPosition(getPosition);
-} catch(error) {
-    console.log(error);
+
+function getLocation() {
+    try {
+        navigator.geolocation.getCurrentPosition(getPosition);
+    } catch(error) {
+        console.log(error);
+    }
 }
 
 function getPosition(position) {
     longitude.innerHTML = position.coords.longitude
     latitude.innerHTML = position.coords.latitude
 }
+
+getBtn.addEventListener('click', ()=> {
+    getLocation()
+})
